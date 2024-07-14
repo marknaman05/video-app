0c182194155be0f5bebb3a3724ca5f0214da4850
marknaman05 <marknaman05@gmail.com>
2024-07-14 23:05:01 +0530

    Made dashboard sidebar ui with route links

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React, { Children, ReactNode } from 'react'
+
+const RootLayout = ({children} : {children: ReactNode}) => {
+  return (
+    
+    <main>
+        
+        {children}
+        
+    </main>
+  )
+}
+
+export default RootLayout
