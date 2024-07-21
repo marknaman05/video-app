b76aa3727237ffbfd30fa3b1ca87df6ae164cb3e
marknaman05 <marknaman05@gmail.com>
2024-07-21 23:41:18 +0530

    added useState for time

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,18 +1,29 @@
+'use client';
+
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
 
   const now = new Date();
-  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-IN', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(now));
+  const [time, setTime] = useState(now);
+
+  const date = (new Intl.DateTimeFormat('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }).format(now));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+        setTime(new Date());
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+    
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
     <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-      <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting at 12:30 PM</h2>
       <div className='flex flex-col gap-2'>
         <h1 className='text-4xl font-extrabold lg:text-7xl'>
-          {time}
+          {time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit'})}
         </h1>
         <p>
           {date}
